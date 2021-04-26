7ea343faa5884756faa7dd5fb0a15c23d13b8461
matro68 <ofriofriofri@gmail.com>
2021-04-26 03:29:21 +0300

    back to toturials, not much accomplished

--- main.py
+++ main.py
@@ -4,19 +4,34 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Line
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.button import Button
 
 kivy.require('2.0.0')
 
 class MainScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = 'main'
+        def func(_):
+            self.parent.current = 'quickgame'
+        self.qg_button = Button(text = 'Quick Game',
+                                font_size = self.height/3,
+                                size_hint = (0.8,0.2),
+                                pos_hint = {'right':0.9,'top':0.8},
+                                on_release = func)
+        self.add_widget(self.qg_button)
 
-class AnotherScreen(Screen):
+    # Button:
+    #     on_release: app.root.current = 'quickgame'
+
+
+class QuickGame(Screen):
     pass
 
 class ScreenManagement(ScreenManager):
     pass
 
-class DrawInput(Widget):
+class Board(Widget):
     def on_touch_down(self, touch):
         # print(touch)
         with self.canvas:
