4b62c80024c76af597bd8fe9a2f78e7508f5021b
matro68 <ofriofriofri@gmail.com>
2021-04-30 09:17:07 +0300

    initial kivy commit

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Line
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
 
+
+from kivy.core.window import Window
+Window.size = (350, 600)
 kivy.require('2.0.0')
 
 class MainScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name = 'main'
-        def func(_):
-            self.parent.current = 'quickgame'
-        self.qg_button = Button(text = 'Quick Game',
-                                font_size = self.height/3,
-                                size_hint = (0.8,0.2),
-                                pos_hint = {'right':0.9,'top':0.8},
-                                on_release = func)
-        self.add_widget(self.qg_button)
-
-    # Button:
-    #     on_release: app.root.current = 'quickgame'
+    pass
+    # def __init__(self, **kwargs):
+    #     super(MainScreen, self).__init__(**kwargs)
 
 
 class QuickGame(Screen):
-    pass
+    board = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(QuickGame, self).__init__(**kwargs)
+        Clock.schedule_once(self.make_board, 0.1)
+
+    def make_board(self,_):
+        for i in range(1,10):
+            self.box = BoxLayout(padding=[2,2])
+            self.grid = GridLayout(cols=3)
+            for j in range(1,10):
+                    self.another_box = BoxLayout(padding=[.8,.8])
+                    self.button = Button(text=str(j),
+                                         color= [0,0,0,1],
+                                         background_color=[1,1,1,1],
+                                         background_normal= '')
+                    self.another_box.add_widget(self.button)
+                    self.grid.add_widget(self.another_box)
+            self.box.add_widget(self.grid)
+            self.board.add_widget(self.box)
+
 
 class ScreenManagement(ScreenManager):
     pass
 
-class Board(Widget):
-    def on_touch_down(self, touch):
-        # print(touch)
-        with self.canvas:
-            touch.ud['line'] = Line(points=(touch.x,touch.y))
-    def on_touch_move(self, touch):
-        # print(touch)
-        touch.ud['line'].points += (touch.x,touch.y)
-    def on_touch_up(self, touch):
-        # print(touch)
-        pass
 
 presenataion = Builder.load_file('Main.kv')
 
